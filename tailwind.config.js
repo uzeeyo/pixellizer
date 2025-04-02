3f73ae55757dab866e4043c17a628d9859fc9571
Jordan Minott <jgminott@gmail.com>
2025-04-02 13:34:15 -0400

    Style changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,21 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       fontFamily: {
-        pixel: ['lilliput', "sans-serif"],
+        pixel: ["lilliput", "monospace"],
+      },
+      keyframes: {
+        pixelMove: {
+          "0%": { backgroundPosition: "0 0, 0 10px, 10px -10px, -10px 0px" },
+          "100%": {
+            backgroundPosition: "20px 0, 20px 10px, 30px -10px, 10px 0px",
+          },
+        },
+      },
+      animation: {
+        pixelMove: "pixelMove 20s linear infinite",
       },
     },
   },
